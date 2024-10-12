1477e6001d3629d55f70f9bef179da765fb506bf
TYG-GHOoST <atangelyt@gmail.com>
2024-10-12 13:46:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "NzN1aGt_Queen-Anya_kcDE=",
+    sessionId: process.env.SESSION_ID || "bWs5eGd_Queen-Anya_uczQ=",
 
     /**
      * thumbnails and medias
