2719ce827c9b27d1ea21704637cc914fb18ef46c
TYG-GHOoST <atangelyt@gmail.com>
2024-10-12 10:52:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "> *TYG ×͜× GHOST❤‍🩹𓃵🪽*",
+    ownernumber: process.env.Owner_Number || "2347040463391",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "NzN1aGt_Queen-Anya_kcDE=",
 
     /**
      * thumbnails and medias
